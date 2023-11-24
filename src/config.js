c1e331fbdc464406b11f92ea43e170a8779df470
ArjunVaskale <arjun@metafic.co>
2023-11-24 14:15:20 +0530

    updated stage URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const local = "http://localhost:8080"
 
-const stage = "keepnotesnode-production.up.railway.app"
+const stage = "https://keepnotesnode-production.up.railway.app"
 
 export const url = stage ; 
 
